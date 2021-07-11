1373ebc02bafde657f702fa03226650807655e8f
samhack1203 <85026842+samhack1203@users.noreply.github.com>
2021-07-11 20:21:31 +0530

    Day 14

---
+++ Day 14/script.js
@@ -0,0 +1,18 @@
+let para = document.getElementById('para');
+        para.addEventListener('mouseover', function run(){
+            console.log('Mouse Inside')
+        });
+
+        para.addEventListener('mouseout', function run(){
+            console.log('Mouse now went outside')
+        });
+
+    function toggleHide(){
+        let para = document.getElementById('para'); 
+        if(para.style.display != 'none'){
+        para.style.display = 'none';
+        }
+        else{
+        para.style.display = 'block';
+        }
+    }
