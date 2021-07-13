4b06babe1faecb85edf08733b8f6cdc633951de2
samhack1203 <85026842+samhack1203@users.noreply.github.com>
2021-07-13 09:04:31 +0530

    Day 16

---
+++ Day 16/Assignment 2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chess from './Chess'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Chess />
+    </div>
+  );
+}
+export default App;
